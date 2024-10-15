be3cf91ebe4f715b52691f9304b4e19118a29a72
Ninaswiftie09 <naj231088@uvg.edu.gt>
2024-10-14 23:06:25 -0600

    Funcionalidad de 1 juagdor terminada

--- Snake.cpp
+++ Snake.cpp
@@ -1,32 +1,87 @@
+// PROYECTO 3, PROGRAMACIÓN DE MICROPROCESADORES
+// NINA NÁJERA - 231088
+// WILSON CALDERÓN - 22018
+// MISHELL CIPRIAN - 231169
+
 #include <iostream>
 #include <thread>
 #include <conio.h>
 #include <windows.h>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
+#include <mutex>
 
 using namespace std;
+
+int modo_juego;  
+mutex mtx;  
+
+struct Coordenada {
+    int x, y;
+};
+
+vector<Coordenada> serpiente;  
+Coordenada comida;  
+bool comida_generada = false;  
+int puntuacion = 0;
+
+const int WIDTH = 50;
+const int HEIGHT = 25;
 
 void Bienvenida() {
     cout << "------------------------------------------------" << endl;
     cout << "              WELCOME TO SNAKE GAME             " << endl;
     cout << "------------------------------------------------" << endl;
-    cout << "Presione cualquier tecla para continuar..." << endl;
-    _getch();
+    cout << "           /\\/\\/\\                              " << endl;
+    cout << "         _|__|  O|                             " << endl;
+    cout << "/~     /~     \\_/ \\                           " << endl;
+    cout << " \\____|__________/  \\                          " << endl;
+    cout << "        \\_______      \\                        " << endl;
+    cout << "                \\     \\                 \\     " << endl;
+    cout << "                  |     |                  \\   " << endl;
+    cout << "                 /      /                    \\ " << endl;
+    cout << "                /     /                       \\" << endl;
+    cout << "              /      /                         \\" << endl;
+    cout << "             /     /                            \\" << endl;
+    cout << "           /     /             _----_            " << endl;
+    cout << "          /     /           _-~      ~-_         " << endl;
+    cout << "         (      (        _-~    _--_    ~-_     _/" << endl;
+    cout << "          \\      ~-____-~    _-~    ~-_    ~-_-~ " << endl;
+    cout << "            ~-_           _-~          ~-_       " << endl;
+    cout << "               ~--______-~                ~-___-~" << endl;
+    cout << "-------------------------------------------------" << endl;
+    cout << "Seleccione su modo de juego" << endl;
+    cout << "1. Un Jugador" << endl;
+    cout << "2. Dos Jugadores" << endl;
+    cin >> modo_juego;
+    
+    switch (modo_juego) {
+        case 1:
+            break;
+        case 2:
+            cout <<" Opcion aun no disponible (Wilson/Mishell)" << endl;
+            system("pause");
+            system("cls");
+            Bienvenida();
+            break;
+        default:
+            cout << "Seleccione una de las dos opciones disponibles." << endl;
+            system("pause");
+            system("cls");
+            Bienvenida();
+            break;
+    }
+    
     system("cls");
 }
 
 void Limites() {
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-    int width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-    int height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            if (i == 0 || i == height - 1) {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (i == 0 || i == HEIGHT - 1) {
                 cout << "-";
-            } else if (j == 0 || j == width - 1) {
+            } else if (j == 0 || j == WIDTH - 1) {
                 cout << "|";
             } else {
                 cout << " ";
@@ -43,6 +98,10 @@ void gotoxy(int x, int y) {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
+void LimpiarPantalla() {
+    system("cls");  
+}
+
 void GameOver() {
     system("cls");
     cout << "   ____      _      __  __  U _____ u       U  ___ u__     __ U _____ u   ____     \n";
@@ -54,52 +113,104 @@ void GameOver() {
     cout << " (__)__) (__)  (__)(./  \\.) (__) (__)         (__)   (__)     (__) (__) (__)  (__) \n";
 }
 
+void Dibuja_Serpiente() {
+    for (size_t i = 0; i < serpiente.size(); i++) {
+        gotoxy(serpiente[i].x, serpiente[i].y);
+        if (i == 0) {
+            cout << "O";  
+        } else {
+            cout << "o";  
+        }
+    }
+}
+
+void MostrarPuntuacion() {
+    gotoxy(0, HEIGHT);
+    cout << "Puntaje: " << puntuacion << endl;  
+}
+
 void Movimiento_Snake() {
     char tecla;
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-    int width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-    int height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-    
-    srand(time(0));
-    int x = rand() % (width - 2) + 1;
-    int y = rand() % (height - 2) + 1;
 
-    gotoxy(x, y);
-    cout << "o";
+    Coordenada inicio = { WIDTH / 2, HEIGHT / 2 };
+    serpiente.push_back(inicio);
 
     while (true) {
+        mtx.lock();
+        LimpiarPantalla();
+        Limites();
+        Dibuja_Serpiente();
+        gotoxy(comida.x, comida.y);
+        cout << "@";  // Comida
+        MostrarPuntuacion();
+        mtx.unlock();
+
         if (_kbhit()) {
             tecla = _getch();
-            gotoxy(x, y);
-            cout << " ";
+            mtx.lock();
 
-            if (tecla == 'w') y--;
-            else if (tecla == 's') y++;
-            else if (tecla == 'a') x--;
-            else if (tecla == 'd') x++;
+            Coordenada nueva_pos = serpiente[0];
 
-            
-            if (x == 0 || x == width - 1 || y == 0 || y == height - 1) {
+            if (tecla == 'w') nueva_pos.y--;
+            else if (tecla == 's') nueva_pos.y++;
+            else if (tecla == 'a') nueva_pos.x--;
+            else if (tecla == 'd') nueva_pos.x++;
+
+            if (nueva_pos.x == 0 || nueva_pos.x == WIDTH - 1 || nueva_pos.y == 0 || nueva_pos.y == HEIGHT - 1) {
                 GameOver();
                 break;
             }
 
-            gotoxy(x, y);
-            cout << "o";
+            if (nueva_pos.x == comida.x && nueva_pos.y == comida.y) {
+                serpiente.push_back(serpiente.back());  
+                puntuacion += 10;  
+                comida_generada = false;  
+            }
+
+            for (size_t i = 1; i < serpiente.size(); i++) {
+                if (nueva_pos.x == serpiente[i].x && nueva_pos.y == serpiente[i].y) {
+                    GameOver();
+                    return;
+                }
+            }
+
+            for (size_t i = serpiente.size() - 1; i > 0; i--) {
+                serpiente[i] = serpiente[i - 1];
+            }
+
+            serpiente[0] = nueva_pos;
+            mtx.unlock();
         }
-        Sleep(100);
+
+        Sleep(100);  
+    }
+}
+
+void* Comida(void*) {
+    srand(time(0));
+
+    while (true) {
+        if (!comida_generada) {  
+            mtx.lock();
+            comida.x = rand() % (WIDTH - 2) + 1;
+            comida.y = rand() % (HEIGHT - 2) + 1;
+            comida_generada = true;  
+            mtx.unlock();
+        }
+        Sleep(5000);  
     }
 }
 
 int main() {
-    thread bienvenida(Bienvenida);
-    bienvenida.join();
+    Bienvenida();
 
     thread limites(Limites);
     limites.join();
 
+    thread comida_thread(Comida, nullptr); 
     thread snake(Movimiento_Snake);
+
+    comida_thread.join();
     snake.join();
 
     return 0;
